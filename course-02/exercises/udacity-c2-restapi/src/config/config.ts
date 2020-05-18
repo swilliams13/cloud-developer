2198e200f9993197f8987b8a889e3bf99c1d20ad
Samantha Williams <swilliams13@gmail.com>
2020-05-18 11:08:39 -0400

    lesson 5.6

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -7,7 +7,8 @@ export const config = {
     "dialect": "postgres",
     "aws_region": process.env.AWS_REGION,
     "aws_profile": process.env.AWS_PROFILE,
-    "aws_media_bucket":  process.env.AWS_MEDIA_BUCKET
+    "aws_media_bucket":  process.env.AWS_MEDIA_BUCKET,
+    
   },
   "prod": {
     "username": "",
@@ -15,5 +16,8 @@ export const config = {
     "database": "udagram_prod",
     "host": "",
     "dialect": "postgres"
+  },
+  "jwt": {
+    "secret":"helloworld"
   }
 }
